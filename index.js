096d5ccb8fe8b165e69879ee706642de225c69a3
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-08-16 21:51:15 -0400

    add babel and use ES2015 synax

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var moment = require('moment');
 
 console.log(moment().format('dddd'));
 
-// load math.js
-import * as math from 'mathjs'
+// // // load math.js
+// import * as math from 'mathjs'
 
-// use math.js
-console.log(math.sqrt(-4)) // 2i
+// // use math.js
+// console.log(math.sqrt(-4)) // 2i
+
+// Interpolate variable bindings
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`)
